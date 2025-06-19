ebeaee2912a54e3cab15c0ca0f85b04fe51c3678
ParvezIAgit config --global user.email codewithparvez@gmail.com <codewithparvez@gmail.com>
2025-06-19 06:18:40 +0530

    first code to let computer select rock paper or scssior - fail

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+function getComputerPlay() {
+    let random = Math.random() * 1;
+    let rock = "Rock";
+    let paper = "Paper";
+    let scissor = "Scissor";
+
+    if (random => 0.33333) {
+        return rock;
+    } else if (random => 0.66666) {
+        return paper;
+    } else {
+        return scissor;
+    }
+}
+
